e96027ca01df6d4ad33cf2d4db8f87ade2bd0618
lishide <lishidezy@qq.com>
2018-03-22 17:02:57 +0800

    fix error

--- app/src/main/java/com/lishide/kotlindp/activity/builder/demo2/Builder.kt
+++ app/src/main/java/com/lishide/kotlindp/activity/builder/demo2/Builder.kt
@@ -3,7 +3,7 @@ package com.lishide.kotlindp.activity.builder.demo2
 /**
  * Created by lishide on 18-3-17.
  * 抽象 Builder 类
- * 定义了三个抽象方法，用于设置产品属性及获取实例。
+ * 定义了5个抽象方法，用于设置产品属性及获取实例。
  */
 abstract class Builder {
     // 设置品牌
