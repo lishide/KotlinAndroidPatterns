80f18c4f657d03d3d5eca99c66514954ce9b359e
lishide <lishidezy@qq.com>
2018-06-04 10:19:39 +0800

    upgrade gradle

--- app/src/main/java/com/lishide/kotlindp/base/BaseViewHolder.kt
+++ app/src/main/java/com/lishide/kotlindp/base/BaseViewHolder.kt
@@ -9,8 +9,8 @@ open class BaseViewHolder(
         context: Context,
         layoutResId: Int,
         parent: ViewGroup,
-        attach: Boolean) : RecyclerView.ViewHolder(
-        LayoutInflater.from(context).inflate(layoutResId, parent, attach)) {
+        attach: Boolean
+) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(layoutResId, parent, attach)) {
 
     constructor(context: Context, layoutResId: Int, container: ViewGroup)
             : this(context, layoutResId, container, false)
